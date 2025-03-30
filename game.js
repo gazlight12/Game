1121bd815753970b986f892d98335bb0263f0273
gazlight12 <theminebozz@gmail.com>
2025-03-30 14:36:45 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,4 @@
+document.getElementById("startButton").addEventListener("click", () => {
+    alert("Game Starting...");
+    // Here, you will load the actual game logic (Phaser.js, Canvas, etc.)
+});
