2f38c4f53a117f3b8f4970a46b5778871799c814
gazlight12 <theminebozz@gmail.com>
2025-03-30 19:59:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -2,8 +2,8 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 400;
+canvas.width = 1600;
+canvas.height = 800;
 
 let gameStarted = false;
 
